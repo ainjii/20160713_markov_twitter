0348195d44624bb352d3d46133000522a1ae0945
Hackbright Student <info@hackbrightacademy.com>
2016-07-13 17:23:04 -0700

    added twitter access for tweeting

--- markov.py
+++ markov.py
@@ -1,79 +1,132 @@
-import os
-import sys
 from random import choice
-import twitter
+from sys import argv
+import os    #to access our OS environment variables
+
+import twitter #available on lab machines, otherwise pip install into active venv
+
+#Using python os.environ to get environtmental variables
+#
+# Note: you must run 'source secrets.sh' before running this file to set reqd env vars
+
+def twitter_funct(text):
+
+    api = twitter.Api(
+        consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+        consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+        access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+        access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+    #print out credentials to ensure correct
+    print api.VerifyCredentials()
+
+    #send a tweet
+    status = api.PostUpdate(text)
+    print status.text
+
+    return None
 
 
-def open_and_read_file(filenames):
-    """Given a list of files, open them, read the text, and return one long
-        string."""
+def open_and_read_file(file_path):
+    """Takes file path as string; returns text as string.
 
-    body = ""
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
 
-    for filename in filenames:
-        text_file = open(filename)
-        body = body + text_file.read()
-        text_file.close()
+    contents = open(file_path).read()
 
-    return body
+    return contents
 
 
-def make_chains(text_string):
-    """Takes input text as string; returns dictionary of markov chains."""
+def make_chains(text_string, n):
+    """Takes input text as string; returns _dictionary_ of markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> make_chains("hi there mary hi there juanita")
+        {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
+    """
 
     chains = {}
 
     words = text_string.split()
 
-    for i in range(len(words) - 2):
-        key = (words[i], words[i + 1])
-        value = words[i + 2]
+    
+    # get keys for chains dictionary
+    for i in range(len(words)-n):
 
-        if key not in chains:
-            chains[key] = []
+        chains_key = tuple(words[i:i+n])
+        # chains = (words[i], words[i+1])
 
-        chains[key].append(value)
+        chains_value = words[i+n]
 
-        # or we could replace the last three lines with:
-        #    chains.setdefault(key, []).append(value)
+        # print chains_key
+        # print chains_value
+    
+        chains[chains_key] = chains.get(chains_key, [])
+        # don't assign this to a value because .append returns none
+        chains[chains_key].append(chains_value)
 
+        # print chains
+    
     return chains
 
 
-def make_text(chains):
+def make_text(chains, n):
     """Takes dictionary of markov chains; returns random text."""
+    
+    cap_keys = [key for key in chains.keys() if key[0] == key[0].capitalize()]
+    key = choice(cap_keys)
 
-    key = choice(chains.keys())
-    words = [key[0], key[1]]
-    while key in chains:
-        # Keep looping until we have a key that isn't in the chains
-        # (which would mean it was the end of our original text)
-        #
-        # Note that for long texts (like a full book), this might mean
-        # it would run for a very long time.
+    
+    text = ""
+    text_since_punctuation = " ".join(key) + " "
 
-        word = choice(chains[key])
-        words.append(word)
-        key = (key[1], word)
+    #limiting output by character count if there's never an end to the string concatenation
+    #could have user specify the default cut off value (characters)
+    #setting cut off value to 140 to limit to Twitter allowable characters
+    while chains.get(key, False) and len(text) < 140:
+        # print text
+        # print key
+        next_word = choice(chains[key])
+        text_since_punctuation += "{} ".format(next_word)
 
-    return " ".join(words)
+        print "length is:", len(text)+len(text_since_punctuation)
+        #check if current string text is < 140 characters
+        if len(text)+len(text_since_punctuation) > 140:
+            break
+        else:
+            if text_since_punctuation.rstrip().endswith(('.', '!', '?', '"', "'", "...", ",")):
+                text += text_since_punctuation
+                text_since_punctuation = ""
+            
+            # start range at 1 because don't need first element in key (already used)
+            key_list = [key[i] for i in range(1, len(key))]
+            key_list.append(next_word)
+
+            key = tuple(key_list)
+            # print key will print out the tuple of 3 elements
 
 
-def tweet(chains):
-    # Use Python os.environ to get at environmental variables
-    # Note: you must run `source secrets.sh` before running this file
-    # to make sure these environmental variables are set.
-    pass
+    return text
 
-# Get the filenames from the user through a command line prompt, ex:
-# python markov.py green-eggs.txt shakespeare.txt
-filenames = sys.argv[1:]
 
-# Open the files and turn them into one long string
-text = open_and_read_file(filenames)
+input_path = argv[1]
+n = int(argv[2])
 
+# Open the file and turn it into one long string
+input_text = open_and_read_file(input_path)
+# print 'got input_text'
 # Get a Markov chain
-chains = make_chains(text)
+chains = make_chains(input_text, n)
+# print 'chains made'
+# Produce random text
+random_text = make_text(chains, n)
+# print 'random_text made'
+print random_text
 
-# Your task is to write a new function tweet, that will take chains as input
-# tweet(chains)
+twitter_funct(random_text)
